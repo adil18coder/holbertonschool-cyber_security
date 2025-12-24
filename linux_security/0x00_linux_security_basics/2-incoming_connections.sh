ba8467299b79c81378cd86f3476685f76188f0b6
Adil Valiyev <avliyev17@gmail.com>
2025-12-24 11:48:18 +0000

    task1

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-sudo iptables -P INPUT DROP
-sudo iptables -A INPUT -i lo -j ACCEPT
-sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
