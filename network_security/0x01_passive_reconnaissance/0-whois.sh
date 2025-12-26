e078358fc2ff8f076913e7301743e1f29f7ee661
Adil Valiyev <avliyev17@gmail.com>
2025-12-26 14:46:25 +0000

    task0

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-<il,"substr($0,index($0,$3))}' > "$1.csv"
+whois "$1"|awk '/^(Registrant|Admin|Tech)/{s=$1}/ (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Email):/{f=$2;gsub(":","",f);print s" "f","substr($0,index($0,$3))(f=="Street"?" ":"")}/ Phone Ext:/{print s" Phone Ext:,"}/ Fax/{print s" "$2","}/ Fax Ext:/{print s" Fax Ext:,"}' >"$1.csv"
+
