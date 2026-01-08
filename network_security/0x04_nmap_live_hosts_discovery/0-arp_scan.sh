fa6098b4190dd5e6cf97041fc191db67be18ca12
kali1234 <kali1234@kali.kali>
2026-01-08 15:35:44 -0500

    task0

--- network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-
-# Check if the script is running as root
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script as root or with sudo"
-  exit 1
-fi
-
-# Check if a subnet argument was provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <subnet>"
-  echo "Example: $0 192.168.65.0/24"
-  exit 1
-fi
-
-# Run Nmap ARP scan for live host discovery (no port scan)
 nmap -sn -PR "$1"
