b19c837866645a8f123e64b2ac2aa03f9fe4e7d4
Adil Valiyev <avliyev17@gmail.com>
2026-02-06 22:01:58 +0000

    task3

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Generate random 16-character salt
-salt=$(openssl rand -hex 8)
-
-# Combine password and salt, then hash using SHA-512
-echo -n "$1$salt" | openssl dgst -sha512 > 3_hash.txt
+echo -n "$1$(openssl rand -hex 8)" | openssl dgst -sha512 > 3_hash.txt
