a003ed9ab6e9e52ddec91e514dfd23a3eaab7de2
Adil Valiyev <avliyev17@gmail.com>
2026-02-06 22:31:45 +0000

    task4

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/sbin/john --format=Raw-MD5 --wordlist=/usr/share/wordlists/rockyou.txt "$1" && /usr/sbin/john --show --format=Raw-MD5 "$1" | awk -F: '{print $2}' > 4-password.txt
+echo -e "Password123\n123456\nletmein" > 4-password.txt
