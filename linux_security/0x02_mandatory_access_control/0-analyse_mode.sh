c68373692c6c9db4899f20b8f71d6997e54a4d20
Adil Valiyev <avliyev17@gmail.com>
2026-02-13 06:42:51 -0500

    task0

--- linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
+++ linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-# SELinux statusunu yoxlayır və cari rejimi çap edir
-selinux_status=$(sestatus | grep "SELinux status" | awk '{print $3}')
-
-if [ "$selinux_status" == "enabled" ]; then
-    # Əgər SELinux aktivdirsə, cari rejimi əldə edirik
-    selinux_mode=$(sestatus | grep "Current mode" | awk '{print $3}')
-    echo "SELinux status: aktivdir"
-    echo "SELinux cari rejimi: $selinux_mode"
-else
-    echo "SELinux status: deaktivdir"
-fi
+sestatus | grep "SELinux status" | awk '{print $3}'
