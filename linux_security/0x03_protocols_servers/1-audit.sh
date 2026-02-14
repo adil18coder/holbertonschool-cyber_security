3d03f8e33bf64a2638b2ba92d0d5cbed9c2487d9
Adil Valiyev <avliyev17@gmail.com>
2026-02-14 16:40:43 -0500

    task1

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E 'KbdInteractiveAuthentication|UsePAM|X11Forwarding|PrintMotd|AcceptEnv|PasswordAuthentication|PermitRootLogin' /etc/ssh/sshd_config
+grep -Ev 'KbdInteractiveAuthentication|UsePAM|X11Forwarding|PrintMotd|AcceptEnv|PasswordAuthentication|PermitRootLogin' /etc/ssh/sshd_config
