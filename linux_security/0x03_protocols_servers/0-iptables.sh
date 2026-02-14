8e02e339b305b840c77c61d7c9f12be1ecf7a14b
Adil Valiyev <avliyev17@gmail.com>
2026-02-14 16:28:29 -0500

    task0

--- linux_security/0x03_protocols_servers/0-iptables.sh
+++ linux_security/0x03_protocols_servers/0-iptables.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-# Skriptin root (administrator) hüquqları ilə işlədiyini yoxlayaq
-if [ "$(id -u)" -ne 0 ]; then
-    echo "Bu skript root hüquqları ilə işləməlidir. Zəhmət olmasa, sudo ilə çalışdırın."
-    exit 1
-fi
-
-# iptables qaydalarını sıralı şəkildə göstərmək
-echo "Cari iptables qaydaları:"
 iptables -L --line-numbers -n
