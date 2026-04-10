a6f8b0377c3f1ff94f51075d1ced9af12ef3a0ab
adil18coder <avliyev17@gmail.com>
2026-04-10 09:14:49 +0000

    task0

--- linux_security/0x04_buffer_overflow/read_write_heap.py
+++ linux_security/0x04_buffer_overflow/read_write_heap.py
@@ -2,7 +2,6 @@
 import sys
 
 if len(sys.argv) != 4:
-    print("Usage: read_write_heap.py pid search_string replace_string")
     sys.exit(1)
 
 pid = sys.argv[1]
@@ -10,10 +9,8 @@ search = sys.argv[2].encode()
 replace = sys.argv[3].encode()
 
 if len(replace) > len(search):
-    print("Error: replace string is longer than search string")
     sys.exit(1)
 
-# heap adresini tap
 with open(f"/proc/{pid}/maps", "r") as f:
     for line in f:
         if "[heap]" in line:
@@ -23,7 +20,6 @@ with open(f"/proc/{pid}/maps", "r") as f:
             end = int(end, 16)
             break
 
-# mem aç və dəyiş
 with open(f"/proc/{pid}/mem", "r+b", 0) as mem:
     mem.seek(start)
     heap = mem.read(end - start)
@@ -31,12 +27,9 @@ with open(f"/proc/{pid}/mem", "r+b", 0) as mem:
     index = heap.find(search)
 
     if index == -1:
-        print("String not found")
         sys.exit(1)
-
-    print(f"Found at {hex(start + index)}")
 
     mem.seek(start + index)
     mem.write(replace.ljust(len(search), b'\x00'))
 
-print("Done")
+print("SUCCESS!")
