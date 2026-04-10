62dcbec76764aa7e3bc45153750a73658974898d
adil18coder <avliyev17@gmail.com>
2026-04-10 09:12:26 +0000

    task0

--- linux_security/0x04_buffer_overflow/read_write_heap.py
+++ linux_security/0x04_buffer_overflow/read_write_heap.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Heap string search and replace in a running process memory"""
-
 import sys
 
 if len(sys.argv) != 4:
+    print("Usage: read_write_heap.py pid search_string replace_string")
     sys.exit(1)
 
 pid = sys.argv[1]
@@ -11,36 +10,33 @@ search = sys.argv[2].encode()
 replace = sys.argv[3].encode()
 
 if len(replace) > len(search):
+    print("Error: replace string is longer than search string")
     sys.exit(1)
 
-heap_start = None
-heap_end = None
-
-# heap region tap
+# heap adresini tap
 with open(f"/proc/{pid}/maps", "r") as f:
     for line in f:
         if "[heap]" in line:
             addr = line.split()[0]
             start, end = addr.split("-")
-            heap_start = int(start, 16)
-            heap_end = int(end, 16)
+            start = int(start, 16)
+            end = int(end, 16)
             break
 
-if heap_start is None:
-    sys.exit(1)
-
-# memory oxu və dəyiş
+# mem aç və dəyiş
 with open(f"/proc/{pid}/mem", "r+b", 0) as mem:
-    mem.seek(heap_start)
-    heap = mem.read(heap_end - heap_start)
+    mem.seek(start)
+    heap = mem.read(end - start)
 
     index = heap.find(search)
 
     if index == -1:
+        print("String not found")
         sys.exit(1)
 
-    mem.seek(heap_start + index)
+    print(f"Found at {hex(start + index)}")
+
+    mem.seek(start + index)
     mem.write(replace.ljust(len(search), b'\x00'))
 
-# checker ONLY bunu istəyir
-print("SUCCESS!")
+print("Done")
