72b4f9678c3200bd5e2b9b2a254c263b09786689
kali <avliyev17>
2026-04-18 07:23:31 -0400

    task3

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 1000 auth.log | grep "Accepted password" | awk '{print $11}' | sort | uniq | wc -l
+tail -n 1000 auth.log | grep "Accepted" | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort | uniq | wc -l
