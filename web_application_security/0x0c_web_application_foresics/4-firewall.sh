8484dfe6d024922419212043ace1b242d2348d22
kali <avliyev17>
2026-04-18 07:55:45 -0400

    task4

--- web_application_security/0x0c_web_application_foresics/4-firewall.sh
+++ web_application_security/0x0c_web_application_foresics/4-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# auth.log faylında firewall qaydalarının əlavə edilməsi sayını tapır
-grep "ufw" auth.log | grep "BLOCK" | wc -l
+# auth.log faylında firewall qaydalarının əlavə edilməsi (iptables) sayını tapır
+grep "iptables" auth.log | wc -l
