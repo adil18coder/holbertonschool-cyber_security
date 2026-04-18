0a5315fa8b7479eb2b490b6698646e051e11d482
kali <avliyev17>
2026-04-18 07:13:27 -0400

    task2

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Loq faylını analiz edir və ən çox uğursuz giriş cəhdi olan istifadəçini tapır
-tail -n 1000 auth.log | grep "Failed password" | awk '{print $(NF-5)}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+# auth.log faylının son 1000 sətrində ən çox uğursuz giriş cəhdi olan istifadəçini tapır
+tail -n 1000 auth.log | grep "Failed password" | awk '{for(i=1;i<=NF;i++) if($i=="for") print $(i+1)}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
