763262fbff793d316d3eee0c5e4fe1d6d83e253d
kali <avliyev17>
2026-04-18 07:33:28 -0400

    task3

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 1000 auth.log | grep "Accepted password" | awk '{print $(NF-3)}' | sort -u | wc -l
+comm -12 <(tail -n 1000 auth.log | grep "Failed password" | sed -n 's/.*from \([0-9.]*\).*/\1/p' | sort -u) <(tail -n 1000 auth.log | grep "Accepted password" | sed -n 's/.*from \([0-9.]*\).*/\1/p' | sort -u) | wc -l
