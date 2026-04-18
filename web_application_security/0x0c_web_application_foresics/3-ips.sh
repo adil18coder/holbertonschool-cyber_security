58ccefc756a6a2b3be469e0837921dbdc5f1bcbe
kali <avliyev17>
2026-04-18 07:29:52 -0400

    task3

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# auth.log faylındakı bütün uğurlu girişlərin unikal IP sayını tapır
+# auth.log faylındakı bütün 'Accepted' sətirlərindən unikal IP-ləri tapır və sayır
 grep "Accepted" auth.log | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sort -u | wc -l
